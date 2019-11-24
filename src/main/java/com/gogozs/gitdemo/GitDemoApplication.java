ca70bc856e3718385f17179a9c676ede02fce16a
architect-s <36916903+architect-s@users.noreply.github.com>
2019-11-24 23:46:51 +0800

    远程修改
    远程修改

--- src/main/java/com/gogozs/gitdemo/GitDemoApplication.java
+++ src/main/java/com/gogozs/gitdemo/GitDemoApplication.java
@@ -3,6 +3,7 @@ package com.gogozs.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//远程修改
 @SpringBootApplication
 public class GitDemoApplication {
 
