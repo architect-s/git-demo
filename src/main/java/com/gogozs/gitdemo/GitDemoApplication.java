e949aec7bd2aca4d7ae8c2127a03fc75110c35e0
czs_glo <czs_glo@czs.com>
2019-11-24 23:47:42 +0800

    idea修改

--- src/main/java/com/gogozs/gitdemo/GitDemoApplication.java
+++ src/main/java/com/gogozs/gitdemo/GitDemoApplication.java
@@ -3,6 +3,8 @@ package com.gogozs.gitdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//idea修改
 @SpringBootApplication
 public class GitDemoApplication {
 
